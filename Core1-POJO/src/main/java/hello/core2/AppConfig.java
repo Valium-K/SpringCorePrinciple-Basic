3659c67aaece7800fd68c16a6efa4e4b5395b5ff
Valium-K <lschan96@gmail.com>
2021-05-16 14:21:40 +0900

    Appconfig적용

---
+++ Core1-POJO/src/main/java/hello/core2/AppConfig.java
@@ -0,0 +1,27 @@
+package hello.core2;
+
+
+import hello.core2.discount.FixDiscountPolicy;
+import hello.core2.member.MemberService;
+import hello.core2.member.MemberServiceImpl;
+import hello.core2.member.MemoryMemberRepository;
+import hello.core2.order.OrderService;
+import hello.core2.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
--- Core1-POJO/src/main/java/hello/core2/member/MemberServiceImpl.java
+++ Core1-POJO/src/main/java/hello/core2/member/MemberServiceImpl.java
@@ -3,7 +3,18 @@ package hello.core2.member;
 // 구현체가 하나만 있을때는 구현체뒤에 Impl을 관례상 쓴다.
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // MemberServiceImpl이 memberRepository를 의존하지만
+    // MemoryMemberRepository를 직접 생성하는 상황 - 사실상 구현체도 의존
+    // 그래서 AppConfig가 이를 담당하게함
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 추상화에만 의존
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Override
     public void join(Member member) {
